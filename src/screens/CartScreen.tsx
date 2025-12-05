3b737a7624e7c0703e6450a9577d42a90747d524
Mykola Kashubyak <kashubyak514@gmail.com>
2025-12-05 11:36:53 +0200

    feat: update UI components in Cart and Home screens to use Typography; modify app.json for userInterfaceStyle

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
+import { Typography } from '../components/ui/Typography'
 import { RootTabScreenProps } from '../types/navigation'
 
 type Props = RootTabScreenProps<'Cart'>
 
 export const CartScreen = ({ route }: Props) => {
 	return (
-		<View className='flex-1 justify-center items-center bg-white'>
-			<Text className='text-2xl font-bold text-green-600'>Cart</Text>
-			<Text className='text-gray-500 mt-2'>Selected items: 0</Text>
+		<View className='flex-1 justify-center items-center bg-background'>
+			<Typography variant='h1' className='text-green-600'>
+				Cart
+			</Typography>
+			<Typography variant='body' className='mt-2'>
+				Selected items: 0
+			</Typography>
 		</View>
 	)
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
+import { Typography } from '../components/ui/Typography'
 import { RootTabScreenProps } from '../types/navigation'
 
 type Props = RootTabScreenProps<'Home'>
 
-export const HomeScreen = ({ navigation, route }: Props) => {
+export const HomeScreen = ({ route }: Props) => {
 	return (
-		<View className='flex-1 justify-center items-center bg-white'>
-			<Text className='text-2xl font-bold text-blue-500'>Screen: {route.name}</Text>
-			<Text className='text-gray-500 mt-2'>Here will be the list of products</Text>
+		<View className='flex-1 justify-center items-center bg-background'>
+			<Typography variant='h1' className='text-blue-500'>
+				Screen: {route.name}
+			</Typography>
+			<Typography variant='body' className='mt-2'>
+				Here will be the list of products
+			</Typography>
 		</View>
 	)
 }
